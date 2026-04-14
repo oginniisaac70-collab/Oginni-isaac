bb2e99ae58ba60ea80d1497700be9dd612e73c25
oginniisaac70-collab <oginniisaac70@gmail.com>
2026-04-14 09:15:44 +0100

    Update Navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,6 +4,16 @@ export default function Navbar(){
     return(
      <nav className="nav">
        <h1>Prince Gaming</h1>
-    </nav>
+
+<ul className="nav-links">
+         <li>Home</li>
+         <li>Games</li>
+         <li>About</li>
+         <li>Contact</li>
+        
+
+       </ul>
+       <button className="Login-btn">Login</button>
+     </nav>
     );
 }         
